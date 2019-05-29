eca0fca57222686c2f64edbef7f827ba9a439331
IMS\Dheveshp <dheveshp@gmail.com>
2019-05-29 12:14:36 +0100

    Initial setup of cloud spawner and cloud collector.

---
+++ JackTheGiant/JackTheGiant/Assets/Scripts/CloudScripts/CloudSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] clouds;
+
+    private float distanceBetweenClouds = 3f;
+
+    private float minX;
+    private float maxX;
+
+    private float lastCloudPositionY;
+
+    private float controlX;
+
+    [SerializeField]
+    private GameObject[] collectables;
+
+    private GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
