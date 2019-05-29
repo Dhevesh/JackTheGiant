7196ef179f2844db21eec80f6b829ad1a735e458
IMS\Dheveshp <dheveshp@gmail.com>
2019-05-29 15:51:13 +0100

    Implemented cloud spawner. Spawns white clouds and dark clouds at random. Implemented camera movement.

--- JackTheGiant/JackTheGiant/Assets/Scripts/CloudScripts/CloudSpawner.cs
+++ JackTheGiant/JackTheGiant/Assets/Scripts/CloudScripts/CloudSpawner.cs
@@ -6,14 +6,17 @@ public class CloudSpawner : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject[] clouds;
+    private GameObject[] whiteClouds;
+    [SerializeField]
+    private GameObject darkCloud;
 
     private float distanceBetweenClouds = 3f;
 
     private float minX;
     private float maxX;
 
-    private float lastCloudPositionY;
+    private float lastCloudPositionY = 0;
+    private float newCloudPositionY = 0;
 
     private float controlX;
 
@@ -22,14 +25,69 @@ public class CloudSpawner : MonoBehaviour
 
     private GameObject player;
 
-    void Start()
+    void FixedUpdate()
     {
+        CreateClouds();
+    }
+    void Awake()
+    {
+        controlX = 0;
+        SetMinMaxX();
+    }
+
+    void SetMinMaxX()
+    {
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+        maxX = bounds.x - 0.5f;
+        minX = -bounds.x + 0.5f;
+    }
+
+    void CreateClouds()
+    {
+        int cloudIndex = Random.Range(0, whiteClouds.Length);
+        int cloudProbability = Random.Range(0, 100);
+        GameObject getCloud;
+        Vector3 temp = whiteClouds[cloudIndex].transform.position;
+        temp.y = newCloudPositionY;
+        switch (controlX)
+        {
+            case 0:
+                temp.x = Random.Range(0.0f, maxX);
+                controlX = 1;
+                break;
+            case 1:
+                temp.x = Random.Range(0.0f, minX);
+                controlX = 2;
+                break;
+            case 2:
+                temp.x = Random.Range(1.0f, maxX);
+                controlX = 3;
+                break;
+            case 3:
+                temp.x = Random.Range(-1.0f, minX);
+                controlX = 0;
+                break;
+        }
+
+        if (cloudProbability < 10)
+            getCloud = darkCloud;
+        else
+            getCloud = whiteClouds[cloudIndex];
+        getCloud.transform.position = temp;
+        GameObject spawnedClouds = Instantiate(getCloud);
+        lastCloudPositionY = getCloud.transform.position.y;
+        newCloudPositionY = lastCloudPositionY - distanceBetweenClouds;
         
     }
 
-
-    void Update()
+    void OnTriggerEnter2D(Collider2D target)
     {
-        
+        if (target.tag == "WhiteCloud")
+        {
+            lastCloudPositionY = target.transform.position.y;
+            CreateClouds();
+        }
     }
+
 }
